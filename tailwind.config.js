9046259d82cf5fb971a73681c4b3d42dd79764ed
isterya <vash.vybor.1@gmail.com>
2024-08-11 11:13:55 +0200

    v0.1: tailwind config and project build configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
    content: ['./src/**/*.{html,js}'],
    theme: {
-      extend: {},
+      screens: {
+         lg: { max: '1999.99px' },
+         md: { max: '991.99px' },
+         sm: { max: '767.99px' },
+         xs: { max: '479.99px' },
+      },
+      extend: {
+         fontFamily: {
+            poppins: ['Poppins', 'sans-serif'],
+            montserrat: ['Montserrat', 'sans-serif'],
+         },
+         backgroundImage: {
+            heroGradient: 'linear-gradient(110deg, #4923b4 0%, #e878cf 100%);',
+         },
+         colors: {
+            blueviolet: '#5027b5',
+            lightgrey: '#666768',
+         },
+      },
    },
    plugins: [],
 };
