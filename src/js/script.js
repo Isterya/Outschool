764d042af1c1572e634cf6c511649207d1999856
isterya <vash.vybor.1@gmail.com>
2024-08-11 20:53:39 +0200

    v0.3: hero section layout completed, script improved, bugs fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
    function toggleMenu() {
       const burger = document.querySelector('#burger'),
-         menu = document.querySelector('#mobile-menu'),
-         body = document.querySelector('body');
+         menu = document.querySelector('#mobile-menu');
 
       burger.addEventListener('click', () => {
          burger.classList.toggle('active');
@@ -10,13 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
          menu.classList.toggle('hidden');
          menu.classList.toggle('flex');
 
-         document.body.style.overflow = 'hidden';
+         if (menu.classList.contains('hidden')) {
+            document.body.style.overflow = '';
+         } else {
+            document.body.style.overflow = 'hidden';
+         }
       });
 
       window.addEventListener('resize', () => {
          if (window.innerWidth > 767.99) {
             menu.classList.add('hidden');
             menu.classList.remove('flex');
+
+            document.body.style.overflow = '';
 
             burger.classList.remove('active');
          }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
    content: ['./src/**/*.{html,js}'],
    theme: {
       screens: {
-         lg: { max: '1999.99px' },
+         lg: { max: '1199.99px' },
          md: { max: '991.99px' },
          sm: { max: '767.99px' },
          xs: { max: '479.99px' },
